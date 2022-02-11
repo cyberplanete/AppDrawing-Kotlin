bd9652dbf802d4e3f0debf9fc20653286321f9eb
Pascal Joret <pascal.joret@gmail.com>
2022-02-11 19:05:13 +0100

    Finalisation de la classe DrawingView.kt. Permettant le tracage d'une ligne avec la souris

--- app/src/main/java/net/cyberplanete/drawingkid/DrawingView.kt
+++ app/src/main/java/net/cyberplanete/drawingkid/DrawingView.kt
@@ -3,19 +3,21 @@ package net.cyberplanete.drawingkid
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
 
 
 /// Cette classe herite de la classe View
 class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) {
 
+    ///Hérite de customPath
     private var mDrawPath: CustomPath? = null;
     private var mCanvasBitmap: Bitmap? = null
     private var mDrawPaint: Paint? = null
     private var mCanvasPaint: Paint? = null
-    private var mBrushSize: Float = 0.toFloat()
+    private var mPinceauTaille: Float = 0.toFloat()
     private var couleur = Color.BLACK
-    private var canvas : Canvas? = null
+    private var canvas: Canvas? = null
 
     init {
         setupDrawing()
@@ -23,17 +25,75 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
 
     private fun setupDrawing() {
         mDrawPaint = Paint()
-        mDrawPath = CustomPath(couleur,mBrushSize)
         mDrawPaint!!.color = couleur
         mDrawPaint!!.style = Paint.Style.STROKE
-        mDrawPaint!!.strokeJoin = Paint.Join.ROUND
-        mDrawPaint!!.strokeCap = Paint.Cap.ROUND
+        mDrawPaint!!.strokeJoin = Paint.Join.BEVEL
+        mDrawPaint!!.strokeCap = Paint.Cap.SQUARE
+
+        mDrawPath = CustomPath(couleur, mPinceauTaille)
         mCanvasPaint = Paint(Paint.DITHER_FLAG)
-        mBrushSize = 20.toFloat()
+        mPinceauTaille = 20.toFloat()
+    }
+
+    /// Called during layout when the size of this view has changed
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        mCanvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
+        canvas = Canvas(mCanvasBitmap!!)
+    }
+
+    ///Fonction permettant de créer une ligne
+    override fun draw(canvas: Canvas) {
+        super.draw(canvas)
+        canvas.drawBitmap(mCanvasBitmap!!, 0f, 0f, mCanvasPaint)
+        if (!mDrawPath!!.isEmpty) {
+            mDrawPaint!!.strokeWidth = mDrawPath!!.epaisseurPinceau
+            canvas.drawPath(mDrawPath!!, mDrawPaint!!)
+        }
+    }
+
+    ///Fonction permettant de gerer les evenements sur l'ecran
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        val touchX = event?.x
+        val touchY = event?.y
+
+        when (event?.action) {
+            MotionEvent.ACTION_DOWN -> {
+                mDrawPath!!.couleur = couleur
+                mDrawPath!!.epaisseurPinceau = mPinceauTaille
+
+                mDrawPath!!.reset()
+
+                if (touchX != null) {
+                    if (touchY != null) {
+                        mDrawPath!!.moveTo(touchX, touchY)
+                    }
+                }
+            }
+
+            MotionEvent.ACTION_MOVE -> {
+                if (touchX != null) {
+                    if (touchY != null) {
+                        mDrawPath!!.lineTo(touchX, touchY)
+                    }
+                }
+            }
+
+            MotionEvent.ACTION_UP -> {
+                mDrawPath = CustomPath(couleur, mPinceauTaille)
+            }
+            ///Pour tous les autres évenements je retourne false
+            else -> return false
+
+        }
+        ///Generally, invalidate() means 'redraw on screen' and results to a call of the view's onDraw() method.
+        // So if something changes and it needs to be reflected on screen, you need to call invalidate()
+        invalidate()
+        return true
     }
 
 
-    internal inner class CustomPath(var couleur: Int, var EpaisseurPinceau: Float) : Path() {
+    internal inner class CustomPath(var couleur: Int, var epaisseurPinceau: Float) : Path() {
 
     }
 
