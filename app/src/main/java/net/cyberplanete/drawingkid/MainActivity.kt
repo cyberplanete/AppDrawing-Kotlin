519d8000e46a0da70ede64c4898b2cd15008c86d
Pascal Joret <pascal.joret@gmail.com>
2022-02-13 15:26:53 +0100

    Creation de binding .. Ajout du dialog_brush_size.xml pour permettre le changement de taille du pinceau

--- app/src/main/java/net/cyberplanete/drawingkid/MainActivity.kt
+++ app/src/main/java/net/cyberplanete/drawingkid/MainActivity.kt
@@ -1,17 +1,67 @@
 package net.cyberplanete.drawingkid
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import net.cyberplanete.drawingkid.databinding.ActivityMainBinding
+import net.cyberplanete.drawingkid.databinding.DialogBrushSizeBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private var drawingView : DrawingView? = null
-
+    //private var drawingView : DrawingView? = null
+    private lateinit var bindingDialogBrushSize: DialogBrushSizeBinding
+    private lateinit var bindingMainActivity:  ActivityMainBinding
+    private lateinit var brushDialog :Dialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        drawingView = findViewById(R.id.drawing_view)
-        drawingView?.setTaillePinceau(20.toFloat())
+        bindingMainActivity = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindingMainActivity.root)
+
+        /// J'intègre le dialog dans la page MainActivity
+        bindingDialogBrushSize = DialogBrushSizeBinding.inflate(layoutInflater)
+        brushDialog = Dialog(this)
+        brushDialog.setContentView(bindingDialogBrushSize.root)
+        brushDialog.setTitle("Taille pinceau: ")
+
+        bindingMainActivity.drawingView?.setTaillePinceau(20.toFloat())
+
+        /// Afficher le dialog des pinceaux
+        val ib_brush =  bindingMainActivity.ibBrush
+        ib_brush.setOnClickListener()
+        {
+            showBrushSizeChooserDialog()
+        }
     }
+
+    private fun showBrushSizeChooserDialog()
+    {
+
+        brushDialog.show()
+
+        val smallBtn = bindingDialogBrushSize.ibSmallBrush
+        val mediumBtn = bindingDialogBrushSize.ibMediumBrush
+        val largeBtn = bindingDialogBrushSize.ibLargeBrush
+
+        smallBtn.setOnClickListener()
+        {
+            bindingMainActivity.drawingView.setTaillePinceau(10.toFloat())
+            brushDialog.dismiss()
+        }
+
+        mediumBtn.setOnClickListener()
+        {
+            bindingMainActivity.drawingView.setTaillePinceau(20.toFloat())
+            brushDialog.dismiss()
+        }
+        largeBtn.setOnClickListener()
+        {
+            bindingMainActivity.drawingView.setTaillePinceau(30.toFloat())
+            brushDialog.dismiss()
+        }
+
+
+    }
+
+
 }
